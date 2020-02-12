1d9ccb948804a01627bf47ca00f09af0a2cacc40
Maria Del Carmen Saldivar <maricarmen.saldivar@outlook.com>
2020-02-12 23:30:57 +0000

    Updated createbuild.cs

--- createbuild.cs
+++ createbuild.cs
@@ -26,7 +26,7 @@ namespace mariasaldivar.Function
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"Ok {name} tus cambios estan deployados")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
